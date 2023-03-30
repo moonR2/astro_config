8318e72510232e2316f8d8e45ad2af4a420b4a78
moonR2 <moonyneverend@gmail.com>
2023-03-30 13:26:02 -0500

    prettierd because is blazingly fast

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,7 +10,7 @@ return {
     config.sources = {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettierd,
       null_ls.builtins.formatting.black,
     }
     return config -- return final config table
