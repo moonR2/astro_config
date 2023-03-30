9b2e80ca0b96eff3ac2cc00e44def7f60d223bb5
moonR2 <moonyneverend@gmail.com>
2023-03-30 11:02:00 -0500

    added local-null-ls lol

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
