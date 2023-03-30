f79a7c7a667127a6ddbb695ae7661e493b2cb083
moonR2 <moonyneverend@gmail.com>
2023-03-30 11:03:23 -0500

    misspellit lol

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -11,7 +11,7 @@ return {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formattting.black,
+      null_ls.builtins.formatting.black,
     }
     return config -- return final config table
   end,
